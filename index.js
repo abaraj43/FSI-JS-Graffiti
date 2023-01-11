35ac03d67b15a79d59c0f69c7f768f6abb886335
Areli <arelibarajas9@gmail.com>
2023-01-10 19:21:56 -0800

    change2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let header = document.querySelector('#page-header')
+
     let header = document.querySelector('#page-header')
     header.style.textAlign = "left"
-let dogImages = document.querySelectorAll('.dog-image')
+
     let dogImages = document.querySelectorAll('.dog-image')
     for(let i = 0; i < dogImages.length; i++){
 	    dogImages[i].style.borderRadius = '50px'
 }
-
+let dogName = document.querySelectorAll('.dog-name')
+    dogName.style.textAlign = 'left'
 
 // Select some elements...
